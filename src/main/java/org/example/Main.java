942de902558c6204926190b043716eab3c71a17d
jaska <jaskaamanbayev2003@gmail.com>
2026-07-18 23:00:21 +0500

    Creating and starting threads with runnable interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,32 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        Thread first = new Thread(new ThreadOne());
+        Thread second = new Thread(new ThreadTwo());
+
+        //starts the thread and its processes
+        first.start();
+        second.start();
+    }
+}
+//implementing runnable interface allows to inherit other base classes and lets to extend anything else
+class ThreadOne implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 10; i++){
+            System.out.println("Thread one: " + i);
+        }
+    }
+}
+
+class ThreadTwo implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i = 0; i<15; i++){
+            System.out.println("Thread two: " + i);
         }
     }
 }
