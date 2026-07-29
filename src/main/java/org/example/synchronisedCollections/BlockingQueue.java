9773fa9d397923fcda7d968cfe224ae007094c9d
GTnoBRAND <jaskaamanbayev2003@gmail.com>
2026-07-29 19:48:56 +0500

    synchronised collections, explanation

---
+++ src/main/java/org/example/synchronisedCollections/BlockingQueue.java
@@ -0,0 +1,16 @@
+package org.example.synchronisedCollections;
+
+
+/**
+ * Synchronised collections are simply thread-safe mechanism that prevents race-condition by allowing only one thread at the time to
+ * execute tasks, so it synchronizes thread ti access shared mutable data, because there is monitor siting behind it, using locks.
+ * threads running in a monitor could be blocked while waiting for certain conditions to be met*/
+public class BlockingQueue {
+    static void main() {
+
+    }
+}
+
+
+
+
