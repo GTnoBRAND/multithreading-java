82968903b43a7b82f1545d0b5e4d9571a42bc9cd
GTnoBRAND <jaskaamanbayev2003@gmail.com>
2026-07-31 01:28:24 +0900

    TransferQueue explanation

---
+++ src/main/java/org/example/synchronisedCollections/TransferQueue.java
@@ -0,0 +1,13 @@
+package org.example.synchronisedCollections;
+
+
+
+/**
+ * Transfer queue is similar to BlockingQueue, but it has that backpressure. It kind of shows producer consumer implementation
+ * when producer thread sends tasks to the queue with transfer(); it blocks until consumer thread retrieves and executes that task*/
+public class TransferQueue {
+
+    static void main() {
+
+    }
+}
