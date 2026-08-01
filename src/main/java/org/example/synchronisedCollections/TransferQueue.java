94fe91ee130d764890bbfe4034e53d28b67dfeb9
Lib <jaskaamanbayev2003@gmail.com>
2026-08-01 22:50:16 +0500

    Document transfer methods in TransferQueue
    Add documentation for transfer methods in TransferQueue.

--- src/main/java/org/example/synchronisedCollections/TransferQueue.java
+++ src/main/java/org/example/synchronisedCollections/TransferQueue.java
@@ -10,4 +10,9 @@ public class TransferQueue {
     static void main() {
 
     }
-}
+/**
+Itransfer(); method enqueuea to an existing queue and waits until consumer consumes it with take(); or poll();.
+tryTranfer(Element e); returns true if any waiting threadin that instant, false if none.
+tryTransfer(Element e, long timeout, TimeUnit.Unit); checks for availqble consumerinstantly, if none exists it waits for specified timeout and onpy then hives up*/
+
+
