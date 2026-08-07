80fcc5b27c62504de65d5e75ad335e7e0b83a6f5
GTnoBRAND <jaskaamanbayev2003@gmail.com>
2026-08-07 22:35:00 +0500

    ConcurrentHashMap description

---
+++ src/main/java/org/example/executorService/concurrentMap/ConcurrentHashMap.java
@@ -0,0 +1,14 @@
+package org.example.executorService.concurrentMap;
+
+
+/**
+ * A ConcurrentHashMap is a high-performance, thread-safe implementation
+ * of the Map interface designed for high-concurrency environments.
+ * It allows multiple threads to read and write data simultaneously without
+ * locking the entire map, making it significantly faster than older synchronized
+ * alternatives like Hashtable or Collections.synchronizedMap()*/
+public class ConcurrentHashMap {
+    static void main() {
+
+    }
+}
