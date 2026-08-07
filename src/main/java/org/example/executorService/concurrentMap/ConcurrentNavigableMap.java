63939995675c9bb15201a9c263d4850196c50e39
GTnoBRAND <jaskaamanbayev2003@gmail.com>
2026-08-07 22:36:41 +0500

    ConcurrentNavigableMap description

---
+++ src/main/java/org/example/executorService/concurrentMap/ConcurrentNavigableMap.java
@@ -0,0 +1,15 @@
+package org.example.executorService.concurrentMap;
+
+
+/**
+ * The ConcurrentNavigableMap in Java is
+ * a thread-safe sub-interface of ConcurrentMap and
+ * NavigableMap. It provides concurrent access, atomic updates,
+ * and sorted key navigation, allowing safe sub-map operations
+ * (subMap, headMap, tailMap) across multiple threads without external locks.
+ * Its primary implementation is ConcurrentSkipListMap.*/
+public class ConcurrentNavigableMap {
+    static void main() {
+
+    }
+}
